2e53b6619c59cacd1ce67fd1bd213a54ca8f40b4
mathieuboudreau <emb6150@gmail.com>
2016-11-14 13:44:02 -0330

    Add function to estimate error in fitted VFA T1 for a given inaccurate b1 value

---
+++ src/t1/estimateVFAT1Error.m
@@ -0,0 +1,20 @@
+function [T1errorVal, measuredSignal, Sy, Sx] = estimateVFAT1Error(T1, TR, FAs, B1ErrorRange)
+%ESTIMATEVFAT1ERROR Estimates the error in fitted VFA T1 value
+%   T1: scalar in ms
+%   TR: scalar in ms
+%   FAs: array in deg
+%   B1ErrorRange: array in relative amplitude. True value is 1.
+
+M0 = 10;
+measuredSignal = vfaSignal(T1, TR, FAs, M0);
+
+for ii=1:length(faErrorRange)
+    Sy = measuredSignal./sind(FAs.*B1ErrorRange(ii));
+    Sx = measuredSignal./tand(FAs.*B1ErrorRange(ii));
+
+    p=polyfit(Sx,Sy,1);
+
+    T1errorVal(ii) = -TR./log(p(1));
+end
+
+end
