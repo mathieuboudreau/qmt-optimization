54f51dfe60692516230520e5034f6bdbdc81ad53
mathieuboudreau <emb6150@gmail.com>
2016-11-14 14:29:11 -0330

    Fix bug: invalid variable name

--- src/t1/estimateVFAT1Error.m
+++ src/t1/estimateVFAT1Error.m
@@ -1,14 +1,14 @@
 function [T1errorVal, measuredSignal, Sy, Sx] = estimateVFAT1Error(T1, TR, FAs, B1ErrorRange)
 %ESTIMATEVFAT1ERROR Estimates the error in fitted VFA T1 value
-%   T1: scalar in ms
-%   TR: scalar in ms
+%   T1: scalar in ms or s
+%   TR: scalar in ms or s
 %   FAs: array in deg
 %   B1ErrorRange: array in relative amplitude. True value is 1.
 
 M0 = 10;
 measuredSignal = vfaSignal(T1, TR, FAs, M0);
 
-for ii=1:length(faErrorRange)
+for ii=1:length(B1ErrorRange)
     Sy = measuredSignal./sind(FAs.*B1ErrorRange(ii));
     Sx = measuredSignal./tand(FAs.*B1ErrorRange(ii));
 
