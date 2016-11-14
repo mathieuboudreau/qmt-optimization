f2c57fce295f12fc5284c7f8db1bd18273df633a
mathieuboudreau <emb6150@gmail.com>
2016-11-14 14:29:49 -0330

    Add B1_VFA case in sensitivity calculation

--- src/sensitivity/calcSensitivity.m
+++ src/sensitivity/calcSensitivity.m
@@ -5,7 +5,7 @@ function [sensitivityOfInterest, zSpectrum, d_zSpectrum, Prot] = calcSensitivity
 %the steady-state converges.
 %
 %   ****************************** args ***********************************
-%   qMT5Params: [F,kf,R1f,T2f,T2r]
+%   qMT5Params: [F,kf,R1f,T2f,T2r] % R1f is in seconds
 %   paramOfInterest: Logical mask for qMT5Param. For example, to do the
 %                    partial derivative relative to kf, the logical mask
 %                    would be [0 1 0 0 0].
@@ -50,7 +50,45 @@ function [sensitivityOfInterest, zSpectrum, d_zSpectrum, Prot] = calcSensitivity
             delete('tmp.mat');
 
     elseif strcmp(paramOfInterest,'B1_VFA')
+            % Vary B1 first
+            trueB1 = 1;
+ 
+            prot = load(protocolFile);
+            prot.Alpha = prot.Alpha * (trueB1 + trueB1 * (deltaPerc/100));
+            prot.Angles = prot.Angles * (trueB1 + trueB1 * (deltaPerc/100));
+ 
+            save('tmp.mat', '-struct', 'prot');
+            % Now vary T1f according to the change we would expect in
+            % VFA T1meas           
+            
+            % Fixed VFA parameters
+            TR = 0.015; % s
+            FAs = [3 20]; % deg
+            B1ErrorRange = trueB1 + trueB1 * (deltaPerc/100);
+ 
+            % Setup conversion values
+            T1r = 1;
+            t1f2t1measParams = [qMT5Params(1) qMT5Params(2) 1/qMT5Params(3) T1r];
 
+            %Get VFA T1 val for dB1
+            trueT1meas = convert_T1f_T1meas(t1f2t1measParams, 't1f_2_t1meas');
+            [dT1meas, ~, ~, ~] = estimateVFAT1Error(trueT1meas, TR, FAs, B1ErrorRange);
+
+            % Get T1f val for the dT1meas
+            t1meas2t1fParams = t1f2t1measParams;
+            t1meas2t1fParams(3) = dT1meas;
+            dT1f = convert_T1f_T1meas(t1meas2t1fParams, 't1meas_2_t1f');
+
+            % Get set of dParams for new dT1f
+            dqMT5Params = qMT5Params;
+            dqMT5Params(3) = 1/dT1f; %Param 3 is supposed to be R1f, so invert.
+            
+            [zSpectrum,      ~]   = generateSignalSPGR(qMT5Params, protocolFile);
+            [d_zSpectrum, Prot]   = generateSignalSPGR(dqMT5Params, 'tmp.mat');
+            sensitivityOfInterest = (d_zSpectrum - zSpectrum) ...
+                                             /            ...
+                                 ((trueB1)*(deltaPerc/100));
+            delete('tmp.mat');
     else
         d_qMT5Params = qMT5Params + (qMT5Params.*paramOfInterest)*(deltaPerc/100);
 
