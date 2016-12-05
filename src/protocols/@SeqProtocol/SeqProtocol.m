995483caac6c674eacd4bd297a6c38de3cf5f053
mathieuboudreau <emb6150@gmail.com>
2016-12-05 15:54:40 -0330

    Create abstract class for sequence protocols

---
+++ src/protocols/@SeqProtocol/SeqProtocol.m
@@ -0,0 +1,25 @@
+classdef (Abstract = true) SeqProtocol < handle
+    %SEQPROTOCOL Abstract class for MRI pulse sequence protocols
+    %
+    %   Save and load methods deal strictly at importing/exporting the
+    %   protocol property from/to external files.
+    %
+    %   getProtocol should give the objects protocol details/variables in a
+    %   human redable format for the user.
+    
+    properties (Abstract = true, Access = protected)
+        id
+        protocol
+    end
+    
+    methods (Abstract = true, Access = public)
+        save(obj, fileName)
+        load(obj, fileName)
+        
+        setID(obj, idName)
+        getID(obj)
+        getProtocol(obj)
+    end
+    
+end
+
