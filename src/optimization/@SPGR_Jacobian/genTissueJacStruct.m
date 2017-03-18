6c179d5488c4faf7eb9e131dca0c23aceaf3d1a1
mathieuboudreau <emb6150@gmail.com>
2017-03-18 17:09:31 -0230

    *Add missed file for previous commit

--- src/optimization/@SPGR_Jacobian/genTissueJacStruct.m
+++ src/optimization/@SPGR_Jacobian/genTissueJacStruct.m
@@ -1,10 +1,9 @@
-function paramStruct = genParamStruct(obj)
-%GENPARAMSTRUCT Generate parameter structure as a container for the
+function tissueJacStruct = genTissueJacStruct(obj)
+%GENTISSUEJACSTRUCT Generate TISSUEparameter structure as a container for the
 %parameter keys, respective values (map type), and respective differentials (map type).
     
-    paramStruct.keys = obj.tissueParamsObj.paramsKeys;
-    
-    paramStruct.values = containers.Map(paramStruct.keys, obj.tissueParamsObj.getParams());
-    paramStruct.differentials = containers.Map(paramStruct.keys, obj.tissueParamsObj.getParams() .* (obj.deltaPerc/100) );
+    tissueJacStruct.keys = obj.tissueParamsObj.fitParamsKeys;
+    tissueJacStruct.value =  containers.Map(tissueJacStruct.keys, obj.tissueParamsObj.getParams(tissueJacStruct.keys));
+    tissueJacStruct.differential =  containers.Map(tissueJacStruct.keys, obj.tissueParamsObj.getParams(tissueJacStruct.keys) .* (obj.deltaPerc/100));
 
 end
