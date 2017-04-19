a9b89efcecfff7c41461ace7a660cb6c4b897d7c
Mathieu Boudreau <emb6150@gmail.com>
2017-04-19 21:00:33 -0230

    Documentation: SPGR_Protocol class

--- src/protocols/@SPGR_Protocol/SPGR_Protocol.m
+++ src/protocols/@SPGR_Protocol/SPGR_Protocol.m
@@ -1,7 +1,35 @@
 classdef SPGR_Protocol < SeqProtocol & AbstractSPGR
-    %SPGR_PROTOCOL SPGR qMT Protocol class
+    %SPGR_PROTOCOL SPGR qMT Protocol class.
     %
-    %   Parent class: SeqProtocol
+    %   --Properties--
+    %   fitProtKeys: Keys representing additional/optional ancillary
+    %                measurements.
+    %
+    %   B1_value: Default (assumed) value for B1 map. Fixed to a value of
+    %            1.0 n.u.
+    %
+    %   --Methods--
+    %   save(fileName): Save protocol to fileName.mat file.
+    %
+    %   load(fileName): Load protocol from fileName.mat file.
+    %
+    %   getProtocol(): Returns protocol.
+    %       *returns*: Struct of protocol values.
+    %
+    %   getNumberOfMeas(): Returns the total number of measurements in a
+    %                      protocol. 
+    %       *returns*: Integer value.
+    %
+    %   getParameter(protKey): Returns qMT SPGR protocol parameters value
+    %                          for given key.
+    %       *returns*: (data type varies on key)
+    %
+    %   remove(indices): Remove sets of angle/offset acquisition points 
+    %                    from the protocol. "indices" must be 1D and of 
+    %                    same length as both protocol.Angles and
+    %                    protocol.Offsets. Non-zero indicies remove 
+    %                    protocol points.
+    %
 
     properties (Access = protected)
     end
