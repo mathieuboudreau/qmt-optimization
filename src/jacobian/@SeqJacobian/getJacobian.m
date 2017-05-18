82ca1a44ab8893108a08c940044becd5e9356872
Mathieu Boudreau <emb6150@gmail.com>
2017-05-18 13:43:25 -0230

    Minor doc improvements

--- src/jacobian/@SeqJacobian/getJacobian.m
+++ src/jacobian/@SeqJacobian/getJacobian.m
@@ -1,6 +1,6 @@
 function jacobianMatrix = getJacobian(obj)
-%GETJACOBIAN Returns the Jacobian which was previously fully calculated for
-%the object's initialized protocol and tissue.
+%GETJACOBIAN Returns the Jacobian matrix which was previously fully 
+%calculated for the object's initialized protocol and tissue.
 
    jacobianMatrix = obj.jacobianStruct.jacobianMatrix; 
 end
