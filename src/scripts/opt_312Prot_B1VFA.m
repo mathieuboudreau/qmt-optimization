574c7aba12ac8e7d43cf8497227560cb15f3268a
Mathieu Boudreau <emb6150@gmail.com>
2017-05-25 16:43:08 -0230

    Add B1-error induced calcs and plot scripts for IR/VFA and two different protocol sets

---
+++ src/scripts/opt_312Prot_B1VFA.m
@@ -0,0 +1,36 @@
+clear all
+load('/Users/mathieuboudreau/Work/Projects/qMT_B1_Optimization_Paper_Analysis/processing/stage_2/part_1/processed_jacobians/312point_protocol.mat')
+opts.fitParams = {'F', 'kf', 'T2f', 'T2r'}
+A=SPGR_Optimization(jacobianObj, opts)
+A.iterOptim
+
+rankedAcqPoints = A.getRankedAcqPoints();
+
+numPoints = length(rankedAcqPoints);
+
+jacStruct = jacobianObj.getJacobianStruct();
+
+for ii = 1:length(opts.fitParams)
+    jacColumnIndices(ii) = find(ismember(jacStruct.paramsKeys, opts.fitParams(ii)));
+end
+
+fitParamJacobian =  jacStruct.jacobianMatrix(:, jacColumnIndices);
+
+b1Jacobian = jacStruct.jacobianMatrix(:,end);
+
+b1ParamError = zeros(numPoints,4);
+
+for ii=numPoints:-1:1
+    
+    tmpParamJacMat = fitParamJacobian(rankedAcqPoints<=ii,:);
+    tmpB1JacMat = b1Jacobian(rankedAcqPoints<=ii);
+
+    
+    b1ParamError(ii, :) = SeqOptimization.minimizeParamErrorBecauseOfMeasError(tmpParamJacMat, tmpB1JacMat, 0.05)'./jacStruct.paramsVals([1 2 5 4]).*100;
+
+end
+
+figure(), plot(b1ParamError(4:end,1))
+figure(), plot(b1ParamError(4:end,2))
+figure(), plot(b1ParamError(4:end,3))
+figure(), plot(b1ParamError(4:end,4))
