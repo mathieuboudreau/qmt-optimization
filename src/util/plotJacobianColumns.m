4a9bcbbe0709665c803a69f67929c12156ce811e
Mathieu Boudreau <emb6150@gmail.com>
2017-06-02 12:15:23 -0230

    Add plot options to jacobian plot; add rainbow line plot external script

--- src/util/plotJacobianColumns.m
+++ src/util/plotJacobianColumns.m
@@ -18,10 +18,33 @@ function plotJacobianColumns(jacobianObject, varargin)
 
     % Each line plotted will be unique to each flip angle
     flipAngles = unique(jacobianStruct.protocol(:,1)); % First column of protocol should be MT FAs for all jacobian rows.
+
+    if nargin>1
+        plotOpts = varargin{1};
+        
+        if isfield(plotOpts, 'lineStyle')
+            lineStyle = plotOpts.lineStyle;
+        end
+        
+        if isfield(plotOpts, 'colorMode')
+            colorMode = plotOpts.colorMode;
+            switch colorMode
+                case 'default'
+                case 'rainbow'
+                    lineColors = linspecer(length(flipAngles));
+                otherwise
+                    error('plotJacobianColumns:unkownColorMode', 'The colorMode specified by the plot options is unkown');
+            end
+        end
+    else
+        lineStyle = '-o'; 
+        colorMode = 'default';
+    end
+    
     
     for paramsIndex = 1:length(jacobianStruct.paramsKeys)
         structHandler.figure = figure();
-
+        
         for faIndex = 1:length(flipAngles)
             offsetIndices = find(jacobianStruct.protocol(:,1)==flipAngles(faIndex));
             offsetValues = jacobianStruct.protocol(offsetIndices,2);
@@ -29,17 +52,27 @@ function plotJacobianColumns(jacobianObject, varargin)
             yValues = jacobianMat(offsetIndices, paramsIndex);
             
             if nargin>1
-                switch varargin{1}
-                    case 'real'
-                        continue
-                    case 'abs'
-                        yValues = abs(jacobianMat(offsetIndices, paramsIndex));
-                    otherwise
-                        continue
-                end
+               plotOpts = varargin{1};
+               if isfield(plotOpts, 'dataMode')          
+                    switch plotOpts.dataMode
+                        case 'real'
+                            continue
+                        case 'abs'
+                            yValues = abs(jacobianMat(offsetIndices, paramsIndex));
+                        otherwise
+                    end
+               end
             end
             
-            semilogx(offsetValues, yValues, 'o-', 'LineWidth', 2 ,'MarkerSize', 10)
+            switch colorMode
+                case 'default'
+                    colorOrder = get(gca, 'ColorOrder');
+                    nextColor = colorOrder(mod(length(get(gca, 'Children')), size(colorOrder, 1))+1, :);
+                case 'rainbow'
+                    nextColor = lineColors(faIndex,:);
+            end
+            
+            semilogx(offsetValues, yValues, lineStyle, 'Color', nextColor, 'LineWidth', 2 ,'MarkerSize', 10);
             hold on
         end
         
